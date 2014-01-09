871dbae48c5d30f5ffb6a06a4f2af7ffa564ec7a
fefifofum <fefifofum@email.com>
2014-01-10 00:25:01 +0100

    Remove stagefright patch

--- full_maxwell2plus.mk
+++ full_maxwell2plus.mk
@@ -1,3 +1,4 @@
+#
 # Copyright (C) 2014 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,15 +12,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 #
+
 # This file is the build configuration for a full Android
 # build for maxwell2plus hardware. This cleanly combines a set of
 # device-specific aspects (drivers) with a device-agnostic
 # product configuration (apps). Except for a few implementation
 # details, it only fundamentally contains two inherit-product
 # lines, full and maxwell2plus, hence its name.
-#
 
 # Inherit from the common open source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
