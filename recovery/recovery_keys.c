30057d5774d717fab5203b213cec8ca548036916
fefifofum <fefifofum@email.com>
2014-03-05 04:16:37 +0100

    Invert volume buttons

--- recovery/recovery_keys.c
+++ recovery/recovery_keys.c
@@ -21,13 +21,13 @@ int device_handle_key(int key_code, int visible) {
     if (visible) {
         switch (key_code) {
             case KEY_CAPSLOCK:
-            case KEY_DOWN:
+            case KEY_UP:
             case KEY_VOLUMEUP:
             case KEY_MENU:
                 return HIGHLIGHT_DOWN;
 
             case KEY_LEFTSHIFT:
-            case KEY_UP:
+            case KEY_DOWN:
             case KEY_VOLUMEDOWN:
             case KEY_HOME:
                 return HIGHLIGHT_UP;
